64125d2bc442cbb7500fc2be5410ae416b170f00
becky <bbaumher@gmail.com>
2016-02-24 16:51:49 -0500

    set testcase file

--- lab2/set_testcase.v
+++ lab2/set_testcase.v
@@ -33,11 +33,11 @@
  * port to rotate the wireframe on the display.
  */
  
-// `define TEST_CASE "test_alu"
+ `define TEST_CASE "test_alu"
 // `define TEST_CASE "test_br"
 // `define TEST_CASE "test_mem"
 // `define TEST_CASE "test_ld_br"
- `define TEST_CASE "test_all"
+// `define TEST_CASE "test_all"
 // `define TEST_CASE "house"
 // `define TEST_CASE "wireframe"
 
